898228c26ca5d63499e8d6a1d61c0a109a8b2626
Alan Wang <wzh19921016@gmail.com>
2024-12-30 15:56:45 -0800

    add spinner

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,28 +1,22 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
+import Spinner from '../Spinner';
 
 function Counter() {
-  const [count, setCount] = React.useState(0);
-  
-  React.useEffect(() => {
-
-    const saved = window.localStorage.getItem('saved-count');
-    if (saved === null) {
-      return;
-    }
-    setCount(Number(saved));
-  }, [])
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    const saved = window.localStorage.getItem("saved-count");
+    setCount(saved === null ? 0 : Number(saved));
+  }, []);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("saved-count", count);
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
-      Count: {count}
+    <button className="count-btn" onClick={() => setCount(count + 1)}>
+      Count: {count === null ? <Spinner /> : count}
     </button>
   );
 }
